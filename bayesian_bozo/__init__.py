628ea11f3335ea81f2ae71729c1e3642bb433988
Justin Bozonier <darkxanthos@gmail.com>
2014-12-29 21:59:49 -0600

    removing broken tests

--- bayesian_bozo/__init__.py
+++ bayesian_bozo/__init__.py
@@ -1,4 +1,4 @@
 #
 from bayesian_bootstrap import bayesian_bootstrap
-from bayesian_bootstrap import test_difference_of_proportions, _test_creating_distribution 
-from bayesian_bootstrap import _compute_exact_lift_data, _create_unimodal_hpd
+from bayesian_bootstrap import test_difference_of_proportions 
+from bayesian_bootstrap import _create_unimodal_hpd
